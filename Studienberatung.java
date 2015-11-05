d7df1e2a9993302ea1062f476fceb1b5d6d56046
Denis Nguyen <you@example.com>
2015-11-05 10:00:27 +0100

    Code von Plass eingefügt

--- Studienberatung.java
+++ Studienberatung.java
@@ -1,8 +1,17 @@
 import java.util.*;
-public class Studienberatung {
-	public static void main (String [] args )	
+public class Studienberatung{
+	public static void main(String[] args){
 		Scanner scanner = new Scanner(System.in);
-	
+		System.out.println("Hast du Abitur?");
+		String antwort = scanner.next();
+		if (antwort.equals("n")){
+			System.out.println("Hast du Fachabitur?");
+			String answerFachabitur = scanner.next();
+			String answer = scanner.next();
+		}
+		if (antwort.equals("j")){
+			System.out.println("Bist du wissbegierig?");
+			String answerWissbegierig = scanner.next();
+		}
 	}
-	
-	}
+}
